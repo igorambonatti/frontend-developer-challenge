3c2b0e082f5f21cf0e713a2ce4a99c75a87c06e0
Igor ambonatti <contatoambonatti@gmail.com>
2020-01-27 23:00:25 -0300

    Implementando Sessão de Produtos

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { Container } from './styles';
+import {
+  Container,
+  ProductsSection,
+  ProductsList,
+  BuyButton,
+  SeekButton,
+} from './styles';
+
+import api from '../../services/api';
 
 import Header from '../../components/Header/index';
 
+import { formatPrice } from '../../util/format';
+
 export default function Home() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    async function handleProducts() {
+      const response = await api.get(
+        'https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1'
+      );
+      const data = response.data.products.map(item => ({
+        ...item,
+        oldFormated: formatPrice(item.oldPrice),
+        priceFormated: formatPrice(item.price),
+        valueFormated: formatPrice(item.installments.value),
+      }));
+      setProducts(data);
+    }
+    handleProducts();
+  }, []);
+
   return (
     <Container>
       <Header />
+      <ProductsSection>
+        {console.log(products)}
+        <h2>Sua seção especial</h2>
+        <ProductsList>
+          {products.map(product => (
+            <li key={product.id}>
+              <img src={product.image} alt="" />
+              <strong>{product.name}</strong>
+              <p>{product.description}</p>
+              <h4>De: {product.oldFormated}</h4>
+              <span>Por: {product.priceFormated}</span>
+              <h5>
+                ou {product.installments.count}x de {product.valueFormated}
+              </h5>
+              <BuyButton>Comprar</BuyButton>
+            </li>
+          ))}
+        </ProductsList>
+        <SeekButton>Ainda mais produtos aqui!</SeekButton>
+      </ProductsSection>
     </Container>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,3 +1,98 @@
 import styled from 'styled-components';
 
 export const Container = styled.div``;
+export const ProductsSection = styled.div`
+  h2 {
+    text-align: center;
+    margin: 49px;
+    color: #888888;
+    font-family: 'Helvetica Neue';
+    font-size: 18px;
+    height: 22px;
+  }
+`;
+export const ProductsList = styled.div`
+  margin: auto;
+  width: 931px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+  list-style: none;
+  li {
+    display: flex;
+    flex-direction: column;
+    border-radius: 4px;
+    height: 350px;
+    width: 200px;
+    margin: 0 0 39px 0;
+    img {
+      border: 1px solid #888888;
+      width: 100%;
+      height: 150px;
+    }
+  }
+  strong {
+    text-decoration: none;
+    font-family: 'Helvetica Neue', sans-serif;
+    font-weight: initial;
+    font-size: 14px;
+    color: #888888;
+
+    margin-top: 14px;
+    height: 16px;
+  }
+  p {
+    font-family: 'Helvetica Neue', sans-serif;
+    color: #888888;
+    font-size: 12px;
+    margin-top: 10px;
+    height: 60px;
+  }
+  h4 {
+    font-size: 12px;
+    color: #888888;
+    font-family: 'Helvetica Neue', sans-serif;
+    font-weight: initial;
+  }
+  span {
+    font-size: 16px;
+    font-weight: bold;
+    font-family: 'Helvetica Neue', sans-serif;
+    color: #888888;
+    margin-top: 6px;
+  }
+  h5 {
+    font-size: 12px;
+    color: #888888;
+    font-family: 'Helvetica Neue', sans-serif;
+    font-weight: initial;
+    margin-top: 7px;
+  }
+`;
+export const BuyButton = styled.button`
+  height: 30px;
+  width: 100%;
+  background: #ffffff;
+  border-radius: 4px;
+  border: 1px solid #707070;
+  font-family: 'Helvetica Neue', sans-serif;
+  color: #707070;
+  margin-top: 16px;
+  font-size: 16px;
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+export const SeekButton = styled.button`
+  display: block;
+  margin: auto;
+  height: 30px;
+  width: 260px;
+  background: #ffffff;
+  border-radius: 4px;
+  border: 1px solid #707070;
+  font-family: 'Helvetica Neue', sans-serif;
+  color: #707070;
+  margin-top: 16px;
+  font-size: 16px;
+`;
