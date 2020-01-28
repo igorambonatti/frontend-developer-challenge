05302aab32a279f04152eeea6fccf16cf7d1daf4
Igor ambonatti <contatoambonatti@gmail.com>
2020-01-28 14:23:52 -0300

    Validação Newsletter

--- src/components/Newsletter/index.js
+++ src/components/Newsletter/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
-import {
-  Container,
-  InputNewsletter,
-  SubmitButton,
-  FormNewsletter,
-} from './styles';
+import * as Yup from 'yup';
+import { Form, Input } from '@rocketseat/unform';
+import { Container, FormContent } from './styles';
 
 export default function Newsletter() {
+  const schema = Yup.object().shape({
+    email: Yup.string()
+      .email('*Insira um email válido')
+      .required('*O email é obrigatório'),
+    user: Yup.string().required('*Insira o nome do seu amigo'),
+  });
+
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
   return (
     <Container>
       <h3>Compartilhe a novidade</h3>
@@ -15,17 +22,19 @@ export default function Newsletter() {
         Quer que seus amigos também ganhem a lista personalizada deles? Preencha
         agora!
       </h4>
-      <FormNewsletter>
-        <InputNewsletter>
-          <span>Nome do seu amigo:</span>
-          <input type="text" />
-        </InputNewsletter>
-        <InputNewsletter>
-          <span>E-mail:</span>
-          <input type="text" />
-        </InputNewsletter>
-      </FormNewsletter>
-      <SubmitButton>Enviar agora</SubmitButton>
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <FormContent>
+          <div>
+            <span>Nome do seu amigo:</span>
+            <Input name="user" />
+          </div>
+          <div>
+            <span>E-mail:</span>
+            <Input name="email" />
+          </div>
+        </FormContent>
+        <button type="submit">Enviar agora</button>
+      </Form>
     </Container>
   );
 }
--- src/components/Newsletter/styles.js
+++ src/components/Newsletter/styles.js
@@ -5,7 +5,6 @@ export const Container = styled.div`
   display: block;
   justify-content: center;
   text-align: center;
-
   h3 {
     font-family: 'Helvetica Neue', sans-serif;
     font-size: 18px;
@@ -20,39 +19,44 @@ export const Container = styled.div`
     color: #888888;
     font-weight: inherit;
   }
-`;
-export const InputNewsletter = styled.div`
-  margin: 0 20px;
-  input {
-    height: 30px;
-    width: 340px;
-    border: 1px solid #707070;
-    font-size: 16px;
-    color: #707070;
-  }
-  span {
-    display: block;
-    text-align: left;
-    font-family: 'Helvetica Neue', sans-serif;
-    font-weight: inherit;
-    color: #888888;
-    height: 14px;
-    font-size: 12px;
-    margin-bottom: 7px;
+  form {
+    button {
+      display: block;
+      margin: auto;
+      height: 39px;
+      width: 260px;
+      border: 1px solid #707070;
+      background: #ffffff;
+      margin-top: 50px;
+      font-size: 16px;
+      color: #888888;
+      border-radius: 4px;
+    }
   }
 `;
-export const SubmitButton = styled.button`
-  height: 39px;
-  width: 260px;
-  border: 1px solid #707070;
-  background: #ffffff;
-  margin-top: 50px;
-  font-size: 16px;
-  color: #888888;
-  border-radius: 4px;
-`;
-export const FormNewsletter = styled.div`
+export const FormContent = styled.div`
   justify-content: center;
   display: flex;
   margin-top: 39px;
+  div {
+    margin: 0 20px;
+    input {
+      background: #fff;
+      height: 30px;
+      width: 340px;
+      border: 1px solid #707070;
+      font-size: 14px;
+      color: #707070;
+    }
+    span {
+      display: block;
+      text-align: left;
+      font-family: 'Helvetica Neue', sans-serif;
+      font-weight: inherit;
+      color: #888888;
+      height: 14px;
+      font-size: 12px;
+      margin-bottom: 7px;
+    }
+  }
 `;
